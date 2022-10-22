02879b36e33255cae3c95503522fee29fc5d198d
Daisuke Fujimura <me@fujimuradaisuke.com>
2022-10-22 17:51:38 +0900

    Show all original contents

--- pbb/src/index.tsx
+++ pbb/src/index.tsx
@@ -25,13 +25,30 @@ type Show = {
 const App = ({ shows }: { shows: Show[] }) => {
   return (
     <div>
-      <h1>Hello!</h1>
-      <img src="./public/logo.jpg" />
+      <img src="./public/logo.jpg" alt="Payback Boys" />
       <ul>
-        {shows.map((show) => (
-          <li>{show.date}</li>
+        {shows.map(({ date, venue }) => (
+          <li key={date}>
+            <span className="live-date">{date}</span>
+            <span className="live-venue">{venue}</span>
+          </li>
         ))}
       </ul>
+      <iframe
+        width="100%"
+        src="https://www.youtube.com/embed/2831-U7zm5A"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        style={{ marginBottom: "10px", minHeight: "315px", border: "0" }}
+      />
+      <iframe
+        style={{ border: 0, width: "100%", height: "472px" }}
+        src="https://bandcamp.com/EmbeddedPlayer/album=3511319946/size=large/bgcol=ffffff/linkcol=0687f5/artwork=small/transparent=true/"
+        seamless
+      >
+        <a href="http://wdsounds.bandcamp.com/album/payback-boys-struggle-for-pride">
+          PAYBACK BOYS &quot;struggle for pride&quot; by PAYBACK BOYS
+        </a>
+      </iframe>
     </div>
   );
 };
@@ -40,11 +57,48 @@ const render = (body: string): string => `
 <!DOCTYPE html>
 <html>
   <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <title>Payback Boys</title>
+    <meta name="description" content="TOKYO STRAIGHT UP HARDCORE">
+    <meta name="author" content="Payback Boys">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="apple-touch-icon" sizes="57x57" href="/public/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="/public/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/public/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="/public/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="/public/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="/public/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="/public/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="/public/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/public/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="/public/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/public/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="/public/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/public/favicon-16x16.png">
+    <link rel="manifest" href="/public/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="/public/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+  <style>
+    #root {
+      text-align: center;
+      max-width: 500px;
+      margin: 0 auto;
+    }
+    #root ul {
+      list-style-type: none;
+        padding: 0;
+    }
+    .live-date {
+      margin-right: 5px;
+      font-weight: bold;
+    }
+  </style>
   </head>
   <body>
+    <div id="root">
     ${body}
+    </div>
   </body>
 </html>
 `;
